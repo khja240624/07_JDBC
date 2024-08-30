9b32d85a32cea3d373f18330a6006a4d1cf63af6
khja240624 <khja240624@gmail.com>
2024-08-30 17:21:06 +0900

    20240830_userManagerment 완성

--- userManagement/src/main/java/edu/kh/jdbc/service/UserServiceImpl.java
+++ userManagement/src/main/java/edu/kh/jdbc/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ package edu.kh.jdbc.service;
 import static edu.kh.jdbc.common.JDBCTemplate.*;
 
 import java.sql.Connection;
+import java.util.List;
 
 import edu.kh.jdbc.dao.UserDao;
 import edu.kh.jdbc.dao.UserDaoImpl;
@@ -65,4 +66,74 @@ public class UserServiceImpl implements UserService{
 		return loginUser;
 	}
 	
+	
+	@Override
+	public List<User> selectAll() throws Exception {
+		
+		Connection conn = getConnection();
+		
+		List<User> userList = dao.selectAll(conn);
+		
+		close(conn);
+		
+		return userList;
+	}
+	
+	
+	@Override
+	public List<User> search(String searchId) throws Exception {
+		
+		// 커넥션 생성
+		Connection conn = getConnection();
+		
+		// 데이터 가공(없으면 패스)
+		searchId = '%' + searchId + '%'; // %검색어%  형태로 가공
+		
+		// DAO 호출 후 결과 반환 받기
+		List<User> userList = dao.search(conn, searchId);
+		
+		close(conn);
+		
+		return userList;
+	}
+	
+	@Override
+	public User selectUesr(String userNo) throws Exception {
+		Connection conn = getConnection();
+		User user = dao.selectUser(conn, userNo);
+		close(conn);
+		return user;
+	}
+	
+	
+	@Override
+	public int deleteUser(int userNo) throws Exception {
+		Connection conn = getConnection();
+		
+		int result = dao.deleteUser(conn, userNo);
+		
+		// 트랜잭션 제어 처리 (dao에서 DML 수행했으니까!!!)
+		if(result > 0)	commit(conn);
+		else			rollback(conn);
+
+		close(conn);
+		
+		return result;
+	}
+	
+	
+	@Override
+	public int updateUser(User user) throws Exception {
+		Connection conn = getConnection();
+		
+		int result = dao.updateUser(conn, user);
+		
+		// 트랜잭션 제어 처리 (dao에서 DML 수행했으니까!!!)
+		if(result > 0)	commit(conn);
+		else			rollback(conn);
+
+		close(conn);
+		
+		return result;
+	}
 }
